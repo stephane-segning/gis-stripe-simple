ac53b5447e6b543f8717c0f754021308891ab819
Stephane Segning Lambou <stephane.segning-lambou@adorsys.com>
2024-12-08 13:16:14 +0100

    added suspense to checkout page

--- src/app/(main)/checkout/page.tsx
+++ src/app/(main)/checkout/page.tsx
@@ -1,20 +1,23 @@
 import CheckoutSummary from "@ss/components/checkout-product/checkout-summary";
 import { CheckoutTemplate } from "@ss/components/checkout-product/checkout-template";
 import { StripeCustomElement } from "@ss/components/stripe-custom-element";
+import { Suspense } from "react";
 
 export default async function CheckoutPage() {
   return (
     <div className="container mx-auto">
-      <CheckoutTemplate>
-        <div className="flex flex-col justify-between gap-4 md:flex-row">
-          <div className="md:basis-1/2 lg:basis-1/3">
-            <CheckoutSummary />
+      <Suspense fallback={<div>Loading...</div>}>
+        <CheckoutTemplate>
+          <div className="flex flex-col justify-between gap-4 md:flex-row">
+            <div className="md:basis-1/2 lg:basis-1/3">
+              <CheckoutSummary />
+            </div>
+            <div className="sticky top-0 md:basis-1/2 lg:basis-1/3">
+              <StripeCustomElement />
+            </div>
           </div>
-          <div className="sticky top-0 md:basis-1/2 lg:basis-1/3">
-            <StripeCustomElement />
-          </div>
-        </div>
-      </CheckoutTemplate>
+        </CheckoutTemplate>
+      </Suspense>
     </div>
   );
 }
